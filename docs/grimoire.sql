b6f9f359f267f1f9739219c0dfa6d9f931b6f673
ddreval <ddreval@053cd13c-6ff2-22de-fc17-e568ec9d4a9c>
2011-10-23 13:51:13 +0000

    dictionary added

--- docs/grimoire.sql
+++ docs/grimoire.sql
@@ -1,167 +1,215 @@
--- todo add cascade deletion for dependent entities
-
-DROP TABLE IF EXISTS transition;
-CREATE TABLE transition (
-       id BIGINT NOT NULL AUTO_INCREMENT,
-       event VARCHAR(32),
-       src_status VARCHAR(32),
-       dest_status VARCHAR(32),
-       handler_name VARCHAR(32),
-       PRIMARY KEY(id)
-);
-
-INSERT INTO transition VALUES (NULL, 'LOAD', 'PERSISTED', 'LOADING', 'uploadHandler');
-INSERT INTO transition VALUES (NULL, 'OK', 'LOADING', 'STORING', 'storeHandler');
-INSERT INTO transition VALUES (NULL, 'FAIL', 'LOADING', 'PERSISTED', 'dummyHandler');
-INSERT INTO transition VALUES (NULL, 'STORE', 'LOADED', 'STORING', 'storeHandler');
-INSERT INTO transition VALUES (NULL, 'STORE', 'STORED', 'STORING', 'storeHandler');
-INSERT INTO transition VALUES (NULL, 'FAIL', 'STORING', 'LOADED', 'dummyHandler');
-INSERT INTO transition VALUES (NULL, 'WAIT', 'STORING', 'LOADED', 'storeHandler');
-INSERT INTO transition VALUES (NULL, 'OK', 'STORING', 'PARSING', 'parseHandler');
-INSERT INTO transition VALUES (NULL, 'PARSE', 'STORED', 'PARSING', 'parseHandler');
-INSERT INTO transition VALUES (NULL, 'PARSE', 'PARSED', 'PARSING', 'parseHandler');
-INSERT INTO transition VALUES (NULL, 'OK', 'PARSING', 'PARSED', 'dummyHandler');
-INSERT INTO transition VALUES (NULL, 'FAIL', 'PARSING', 'STORED', 'dummyHandler');
-INSERT INTO transition VALUES (NULL, 'TRANSLATE', 'PARSED', 'TRANSLATING', 'translateHandler');
-INSERT INTO transition VALUES (NULL, 'TRANSLATE', 'TRANSLATED', 'TRANSLATING', 'translateHandler');
-INSERT INTO transition VALUES (NULL, 'OK', 'TRANSLATING', 'TRANSLATED', 'dummyHandler');
-INSERT INTO transition VALUES (NULL, 'FAIL', 'TRANSLATING', 'PARSED', 'dummyHandler');
-
-DROP TABLE IF EXISTS job;
-CREATE TABLE job (
-       id BIGINT NOT NULL AUTO_INCREMENT,
-       trans_id BIGINT NOT NULL,
-       dict_id BIGINT NOT NULL,
-       progress INT NOT NULL DEFAULT 0,
-       details VARCHAR(255),
-       active BOOL NOT NULL DEFAULT FALSE,
-       PRIMARY KEY(id)
-);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
 
--- CREATE TABLE BespokeWaitConditionParameter (
---   id BIGINT(20) NOT NULL AUTO_INCREMENT,
---   bespokeWaitConditionId BIGINT(20) NULL,
---   name VARCHAR(255) NULL,
---   description VARCHAR(255) NULL,
---   PRIMARY KEY(id),
---   UNIQUE INDEX BespokeWaitConditionParameter_AK(bespokeWaitConditionId, name),
---   FOREIGN KEY(bespokeWaitConditionId)
---     REFERENCES BespokeWaitCondition(id)
---       ON DELETE CASCADE
---       ON UPDATE CASCADE
--- )
+-- -----------------------------------------------------
+-- Table `transition`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `transition` ;
 
-DROP TABLE IF EXISTS lang;
-CREATE TABLE lang (
-  id VARCHAR(2) NULL,
-  name VARCHAR(16) NULL,
-  encodings VARCHAR(255),
-  PRIMARY KEY(id)
-);
+CREATE  TABLE IF NOT EXISTS `transition` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT ,
+  `event` VARCHAR(32) NULL DEFAULT NULL ,
+  `src_status` VARCHAR(32) NULL DEFAULT NULL ,
+  `dest_status` VARCHAR(32) NULL DEFAULT NULL ,
+  `handler_name` VARCHAR(32) NULL DEFAULT NULL ,
+  PRIMARY KEY (`id`) );
 
-INSERT INTO lang VALUES ('ru', 'Russian', 'Windows-1251,KOI8-R,Cp866,UTF-8');
-INSERT INTO lang VALUES ('zh', 'Chinese', 'UTF-8');
-INSERT INTO lang VALUES ('en', 'English', 'ISO-8859-1,UTF-8');
-INSERT INTO lang VALUES ('fi', 'Finnish', 'ISO-8859-1,UTF-8');
-INSERT INTO lang VALUES ('fr', 'French', 'ISO-8859-1,UTF-8');
-INSERT INTO lang VALUES ('de', 'German', 'ISO-8859-1,UTF-8');
-INSERT INTO lang VALUES ('el', 'Greek', 'UTF-8');
-INSERT INTO lang VALUES ('is', 'Icelandic', 'ISO-8859-1,UTF-8');
-INSERT INTO lang VALUES ('ja', 'Japanese', 'UTF-8');
-INSERT INTO lang VALUES ('es', 'Spanish', 'ISO-8859-1,UTF-8');
-INSERT INTO lang VALUES ('sv', 'Swedish', 'ISO-8859-1,UTF-8');
-INSERT INTO lang VALUES ('ar', 'Arabic', 'UTF-8');
 
-DROP TABLE IF EXISTS service;
-CREATE TABLE service (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(32) NULL,
-  host VARCHAR(64),
-  req_pattern VARCHAR(255) NULL,
-  res_pattern VARCHAR(255) NULL,
-  PRIMARY KEY(id)
-);
+-- -----------------------------------------------------
+-- Table `lang`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `lang` ;
 
-INSERT INTO service VALUES (NULL,'Google','translate.google.com','/translate_t?hl=is&ie=UTF-8&text="+str+"&sl=en&tl=is#',
-       '<span id=result_box class=\"short_text\"><span[\\s\\w+=\"w+|.+#+\"]+>(\\w+)');
+CREATE  TABLE IF NOT EXISTS `lang` (
+  `id` VARCHAR(2) NULL DEFAULT NULL ,
+  `name` VARCHAR(16) NULL DEFAULT NULL ,
+  `encodings` VARCHAR(255) NULL DEFAULT NULL ,
+  PRIMARY KEY (`id`) );
 
-DROP TABLE IF EXISTS translation;
-CREATE TABLE translation (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  translators_service_id BIGINT NOT NULL,
-  word_id BIGINT NOT NULL,
-  translator_id BIGINT NOT NULL,
-  value VARCHAR(64) NULL,
-  PRIMARY KEY(id),
-  INDEX translations_FKIndex1(translator_id, translators_service_id),
-  INDEX translations_FKIndex2(word_id)
-);
 
-DROP TABLE IF EXISTS translator;
-CREATE TABLE translator (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  service_id BIGINT NOT NULL,
-  dest_lang_id VARCHAR(2) NOT NULL,
-  src_lang_id VARCHAR(2) NOT NULL,
-  PRIMARY KEY(id, service_id),
-  INDEX lang_translator_FKIndex1(src_lang_id),
-  INDEX lang_translator_FKIndex2(dest_lang_id),
-  INDEX lang_translators_FKIndex3(service_id)
-);
+-- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user` ;
 
-INSERT INTO translator VALUES (1, 1, 1, 3);
+CREATE  TABLE IF NOT EXISTS `user` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT ,
+  `username` VARCHAR(32) NOT NULL ,
+  `password` VARCHAR(32) NOT NULL ,
+  `role` VARCHAR(8) NOT NULL ,
+  `enabled` TINYINT(1)  NOT NULL DEFAULT TRUE ,
+  PRIMARY KEY (`id`) );
 
-DROP TABLE IF EXISTS user;
-CREATE TABLE user (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  username VARCHAR(32) NOT NULL,
-  password VARCHAR(32) NOT NULL,
-  role VARCHAR(8) NOT NULL,
-  enabled BOOL NOT NULL DEFAULT TRUE,
-  PRIMARY KEY(id)
-);
 
-INSERT INTO user VALUES (NULL, 'guest', '', 'GUEST', 1);
-INSERT INTO user VALUES (NULL, 'user', 'pass', 'USER', 1);
-INSERT INTO user VALUES (NULL, 'helicobacter', 'pilory', 'USER', 1);
-INSERT INTO user VALUES (NULL, 'admin', 'admin', 'ADMIN', 1);
+-- -----------------------------------------------------
+-- Table `dict`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `dict` ;
 
-DROP TABLE IF EXISTS dict_word;
-CREATE TABLE dict_word (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  dict_id BIGINT NOT NULL,
-  word_id BIGINT NOT NULL,
-  counter BIGINT NOT NULL,
-  PRIMARY KEY(id),
---  PRIMARY KEY(id, dict_id, word_id),
-  FOREIGN KEY (dict_id) REFERENCES dict(id) ON DELETE CASCADE
---   FOREIGN KEY (word_id) REFERENCES word(id) ON DELETE CASCADE
---   INDEX dict_word_FKIndex1(dict_id),
---   INDEX dict_word_FKIndex2(word_id)
-);
+CREATE  TABLE IF NOT EXISTS `dict` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(128) NULL DEFAULT NULL ,
+  `orig_doc` LONGBLOB NULL DEFAULT NULL ,
+  `utf8_text` LONGTEXT NULL DEFAULT NULL ,
+  `status` VARCHAR(32) NULL DEFAULT NULL ,
+  `encoding` VARCHAR(32) NULL DEFAULT NULL ,
+  `preview` BLOB NULL DEFAULT NULL ,
+  `lang_id` VARCHAR(2) NOT NULL ,
+  `user_id` BIGINT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_dict_lang1` (`lang_id` ASC) ,
+  INDEX `fk_dict_user1` (`user_id` ASC) ,
+  CONSTRAINT `fk_dict_lang1`
+    FOREIGN KEY (`lang_id` )
+    REFERENCES `lang` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_dict_user1`
+    FOREIGN KEY (`user_id` )
+    REFERENCES `user` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
 
-DROP TABLE IF EXISTS word;
-CREATE TABLE word (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  lang_id VARCHAR(2) NOT NULL,
-  value VARCHAR(32) NOT NULL,
-  PRIMARY KEY(id),
-  INDEX words_FKIndex1(lang_id),
-  UNIQUE INDEX words_Unique(value, lang_id)
-);
 
-DROP TABLE IF EXISTS dict;
-CREATE TABLE dict (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  user_id BIGINT NOT NULL,
-  lang_id VARCHAR(2),
-  name VARCHAR(128),
-  orig_doc LONGBLOB,
-  utf8_text LONGTEXT,
-  status VARCHAR(32),
-  encoding VARCHAR(32),
-  preview BLOB,
-  PRIMARY KEY(id),
-  INDEX dicts_FKIndex2(user_id)
-);
+-- -----------------------------------------------------
+-- Table `job`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `job` ;
+
+CREATE  TABLE IF NOT EXISTS `job` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT ,
+  `progress` INT NOT NULL DEFAULT 0 ,
+  `details` VARCHAR(255) NULL DEFAULT NULL ,
+  `active` TINYINT(1)  NOT NULL DEFAULT FALSE ,
+  `transition_id` BIGINT NOT NULL ,
+  `dict_id` BIGINT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_job_transition1` (`transition_id` ASC) ,
+  INDEX `fk_job_dict1` (`dict_id` ASC) ,
+  CONSTRAINT `fk_job_transition1`
+    FOREIGN KEY (`transition_id` )
+    REFERENCES `transition` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_job_dict1`
+    FOREIGN KEY (`dict_id` )
+    REFERENCES `dict` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table `service`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `service` ;
+
+CREATE  TABLE IF NOT EXISTS `service` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT ,
+  `title` VARCHAR(32) NULL DEFAULT NULL ,
+  `host` VARCHAR(64) NULL DEFAULT NULL ,
+  `req_pattern` VARCHAR(255) NULL DEFAULT NULL ,
+  `res_pattern` VARCHAR(255) NULL DEFAULT NULL ,
+  PRIMARY KEY (`id`) );
+
+
+-- -----------------------------------------------------
+-- Table `word`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `word` ;
+
+CREATE  TABLE IF NOT EXISTS `word` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT ,
+  `lang_id` VARCHAR(2) NOT NULL ,
+  `value` VARCHAR(32) NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `words_Unique` (`value` ASC, `lang_id` ASC) );
+
+
+-- -----------------------------------------------------
+-- Table `translator`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `translator` ;
+
+CREATE  TABLE IF NOT EXISTS `translator` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT ,
+  `service_id` BIGINT NOT NULL ,
+  `src_lang_id` VARCHAR(2) NOT NULL ,
+  `dest_lang_id` VARCHAR(2) NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_translator_service1` (`service_id` ASC) ,
+  INDEX `fk_translator_src_lang1` (`src_lang_id` ASC) ,
+  INDEX `fk_translator_dest_lang2` (`dest_lang_id` ASC) ,
+  CONSTRAINT `fk_translator_service1`
+    FOREIGN KEY (`service_id` )
+    REFERENCES `service` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_translator_src_lang1`
+    FOREIGN KEY (`src_lang_id` )
+    REFERENCES `lang` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_translator_dest_lang2`
+    FOREIGN KEY (`dest_lang_id` )
+    REFERENCES `lang` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table `translation`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `translation` ;
+
+CREATE  TABLE IF NOT EXISTS `translation` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT ,
+  `value` VARCHAR(64) NULL DEFAULT NULL ,
+  `word_id` BIGINT NOT NULL ,
+  `translator_id` BIGINT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_translation_word1` (`word_id` ASC) ,
+  INDEX `fk_translation_translator1` (`translator_id` ASC) ,
+  UNIQUE INDEX `word_id_UNIQUE` (`word_id` ASC, `translator_id` ASC) ,
+  CONSTRAINT `fk_translation_word1`
+    FOREIGN KEY (`word_id` )
+    REFERENCES `word` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_translation_translator1`
+    FOREIGN KEY (`translator_id` )
+    REFERENCES `translator` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table `dict_word`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `dict_word` ;
+
+CREATE  TABLE IF NOT EXISTS `dict_word` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT ,
+  `counter` BIGINT NOT NULL ,
+  `dict_id` BIGINT NOT NULL ,
+  `word_id` BIGINT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_dict_word_dict1` (`dict_id` ASC) ,
+  INDEX `fk_dict_word_word1` (`word_id` ASC) ,
+  CONSTRAINT `fk_dict_word_dict1`
+    FOREIGN KEY (`dict_id` )
+    REFERENCES `dict` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_dict_word_word1`
+    FOREIGN KEY (`word_id` )
+    REFERENCES `word` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION);
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
