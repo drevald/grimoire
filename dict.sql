bcde5f5728b287837e84fa43eb75277d4771f2a0
unknown <varra2@yandex.ru>
2021-10-26 22:00:43 +0300

    work done

--- dict.sql
+++ dict.sql
@@ -1,70 +1,91 @@
-INSERT INTO transition VALUES (NULL, 'LOAD', 'PERSISTED', 'LOADING', 'uploadHandler');
-INSERT INTO transition VALUES (NULL, 'OK', 'LOADING', 'STORING', 'storeHandler');
-INSERT INTO transition VALUES (NULL, 'FAIL', 'LOADING', 'PERSISTED', 'dummyHandler');
-INSERT INTO transition VALUES (NULL, 'STORE', 'LOADED', 'STORING', 'storeHandler');
-INSERT INTO transition VALUES (NULL, 'STORE', 'STORED', 'STORING', 'storeHandler');
-INSERT INTO transition VALUES (NULL, 'FAIL', 'STORING', 'LOADED', 'dummyHandler');
-INSERT INTO transition VALUES (NULL, 'WAIT', 'STORING', 'LOADED', 'storeHandler');
-INSERT INTO transition VALUES (NULL, 'OK', 'STORING', 'PARSING', 'parseHandler');
-INSERT INTO transition VALUES (NULL, 'PARSE', 'STORED', 'PARSING', 'parseHandler');
-INSERT INTO transition VALUES (NULL, 'PARSE', 'PARSED', 'PARSING', 'parseHandler');
-INSERT INTO transition VALUES (NULL, 'OK', 'PARSING', 'PARSED', 'dummyHandler');
-INSERT INTO transition VALUES (NULL, 'FAIL', 'PARSING', 'STORED', 'dummyHandler');
-INSERT INTO transition VALUES (NULL, 'TRANSLATE', 'PARSED', 'TRANSLATING', 'translateHandler');
-INSERT INTO transition VALUES (NULL, 'TRANSLATE', 'TRANSLATED', 'TRANSLATING', 'translateHandler');
-INSERT INTO transition VALUES (NULL, 'OK', 'TRANSLATING', 'TRANSLATED', 'dummyHandler');
-INSERT INTO transition VALUES (NULL, 'FAIL', 'TRANSLATING', 'PARSED', 'dummyHandler');
+INSERT INTO transition(event, src_status, dest_status, handler_name)
+	VALUES ('LOAD', 'PERSISTED', 'LOADING', 'uploadHandler');
+INSERT INTO transition(event, src_status, dest_status, handler_name) 
+	VALUES ('OK', 'LOADING', 'STORING', 'storeHandler');
+INSERT INTO transition(event, src_status, dest_status, handler_name) 
+	VALUES ('FAIL', 'LOADING', 'PERSISTED', 'dummyHandler');
+INSERT INTO transition(event, src_status, dest_status, handler_name) 
+	VALUES ('STORE', 'LOADED', 'STORING', 'storeHandler');
+INSERT INTO transition(event, src_status, dest_status, handler_name) 
+	VALUES ('STORE', 'STORED', 'STORING', 'storeHandler');
+INSERT INTO transition(event, src_status, dest_status, handler_name)
+	VALUES ('FAIL', 'STORING', 'LOADED', 'dummyHandler');
+INSERT INTO transition(event, src_status, dest_status, handler_name) 
+	VALUES ('WAIT', 'STORING', 'LOADED', 'storeHandler');
+INSERT INTO transition(event, src_status, dest_status, handler_name) 
+	VALUES ('OK', 'STORING', 'PARSING', 'parseHandler');
+INSERT INTO transition(event, src_status, dest_status, handler_name) 
+	VALUES ('PARSE', 'STORED', 'PARSING', 'parseHandler');
+INSERT INTO transition(event, src_status, dest_status, handler_name) 
+	VALUES ('PARSE', 'PARSED', 'PARSING', 'parseHandler');
+INSERT INTO transition(event, src_status, dest_status, handler_name) 
+	VALUES ('OK', 'PARSING', 'PARSED', 'dummyHandler');
+INSERT INTO transition(event, src_status, dest_status, handler_name) 
+	VALUES ('FAIL', 'PARSING', 'STORED', 'dummyHandler');
+INSERT INTO transition(event, src_status, dest_status, handler_name) 
+	VALUES ('TRANSLATE', 'PARSED', 'TRANSLATING', 'translateHandler');
+INSERT INTO transition(event, src_status, dest_status, handler_name) 
+	VALUES ('TRANSLATE', 'TRANSLATED', 'TRANSLATING', 'translateHandler');
+INSERT INTO transition(event, src_status, dest_status, handler_name) 
+	VALUES ('OK', 'TRANSLATING', 'TRANSLATED', 'dummyHandler');
+INSERT INTO transition(event, src_status, dest_status, handler_name) 
+	VALUES ('FAIL', 'TRANSLATING', 'PARSED', 'dummyHandler');
 
 
-INSERT INTO lang VALUES ('ru', 'Russian', 'Windows-1251,KOI8-R,Cp866,UTF-8');
-INSERT INTO lang VALUES ('zh', 'Chinese', 'UTF-8');
-INSERT INTO lang VALUES ('en', 'English', 'ISO-8859-1,UTF-8');
-INSERT INTO lang VALUES ('fi', 'Finnish', 'ISO-8859-1,UTF-8');
-INSERT INTO lang VALUES ('fr', 'French', 'ISO-8859-1,UTF-8');
-INSERT INTO lang VALUES ('de', 'German', 'ISO-8859-1,UTF-8');
-INSERT INTO lang VALUES ('el', 'Greek', 'UTF-8');
-INSERT INTO lang VALUES ('is', 'Icelandic', 'ISO-8859-1,UTF-8');
-INSERT INTO lang VALUES ('ja', 'Japanese', 'UTF-8');
-INSERT INTO lang VALUES ('es', 'Spanish', 'ISO-8859-1,UTF-8');
-INSERT INTO lang VALUES ('sv', 'Swedish', 'ISO-8859-1,UTF-8');
-INSERT INTO lang VALUES ('ar', 'Arabic', 'UTF-8');
-INSERT INTO lang VALUES ('la', 'Latin', 'ISO-8859-1');
-INSERT INTO lang VALUES ('da', 'Danish', 'ISO-8859-1,UTF-8');
-INSERT INTO lang VALUES ('pl', 'Polish', 'UTF-8,Windows-1250,ISO-8859-2');
+INSERT INTO lang(id,"name",encodings) VALUES ('ru', 'Russian', 'Windows-1251,KOI8-R,Cp866,UTF-8');
+INSERT INTO lang(id,"name",encodings) VALUES ('zh', 'Chinese', 'UTF-8');
+INSERT INTO lang(id,"name",encodings) VALUES ('en', 'English', 'ISO-8859-1,UTF-8');
+INSERT INTO lang(id,"name",encodings) VALUES ('fi', 'Finnish', 'ISO-8859-1,UTF-8');
+INSERT INTO lang(id,"name",encodings) VALUES ('fr', 'French', 'ISO-8859-1,UTF-8');
+INSERT INTO lang(id,"name",encodings) VALUES ('de', 'German', 'ISO-8859-1,UTF-8');
+INSERT INTO lang(id,"name",encodings) VALUES ('el', 'Greek', 'UTF-8');
+INSERT INTO lang(id,"name",encodings) VALUES ('is', 'Icelandic', 'ISO-8859-1,UTF-8');
+INSERT INTO lang(id,"name",encodings) VALUES ('ja', 'Japanese', 'UTF-8');
+INSERT INTO lang(id,"name",encodings) VALUES ('es', 'Spanish', 'ISO-8859-1,UTF-8');
+INSERT INTO lang(id,"name",encodings) VALUES ('sv', 'Swedish', 'ISO-8859-1,UTF-8');
+INSERT INTO lang(id,"name",encodings) VALUES ('ar', 'Arabic', 'UTF-8');
+INSERT INTO lang(id,"name",encodings) VALUES ('la', 'Latin', 'ISO-8859-1');
+INSERT INTO lang(id,"name",encodings) VALUES ('da', 'Danish', 'ISO-8859-1,UTF-8');
+INSERT INTO lang(id,"name",encodings) VALUES ('pl', 'Polish', 'UTF-8,Windows-1250,ISO-8859-2');
 
-INSERT INTO user VALUES (NULL, 'guest', '', 'GUEST', 1, 'en');
-INSERT INTO user VALUES (NULL, 'user', 'pass', 'USER', 1, 'en');
-INSERT INTO user VALUES (NULL, 'helicobacter', 'pilory', 'USER', 1, 'en');
-INSERT INTO user VALUES (NULL, 'admin', 'admin', 'ADMIN', 1, 'en');
+INSERT INTO "user"(username, password, role, enabled, native_lang_id) 
+	VALUES ('guest', '', 'GUEST', True, 'en');
+INSERT INTO "user"(username, password, role, enabled, native_lang_id) 
+	VALUES ('user', 'pass', 'USER', True, 'en');
+INSERT INTO "user"(username, password, role, enabled,native_lang_id) 
+	VALUES ('helicobacter', 'pilory', 'USER', True, 'en');
+INSERT INTO "user"(username, password, role, enabled, native_lang_id) 
+	VALUES ('admin', 'admin', 'ADMIN', True, 'en');
 
-INSERT INTO translator_provider VALUES (0, 'Bing', 'api.microsofttranslator.com', '<{1}>{0}</{2}>',
+INSERT INTO translator_provider(id, title, host, req_pattern, res_pattern) 
+	VALUES (0, 'Bing', 'api.microsofttranslator.com', '<{1}>{0}</{2}>',
 'http://api.microsofttranslator.com/V2/Http.svc/Translate?appId=CDCB8BFFDD9E4C3054316BC629E82D1E39CA585C&text={0}&from={1}&to={2}');
-INSERT INTO `translator_provider` VALUES (1,'Google','',
+INSERT INTO translator_provider(id, title, host, req_pattern, res_pattern) 
+	VALUES (1,'Google','',
 '{1}<span id=result_box class=\"short_text\"><span {2}>{0}</span>{3}','http://translate.google.com/?sl={1}&tl={2}&text={0}');
 
-INSERT INTO translator VALUES (0, 0, 'en', 'ru');
-INSERT INTO translator VALUES (1, 0, 'fr', 'ru');
-INSERT INTO translator VALUES (2, 0, 'de', 'ru');
-INSERT INTO translator VALUES (3, 1, 'en', 'ru');
-INSERT INTO translator VALUES (4, 1, 'fr', 'ru');
-INSERT INTO translator VALUES (5, 1, 'de', 'ru');
-INSERT INTO translator VALUES (6, 1, 'el', 'ru');
-INSERT INTO translator VALUES (7, 1, 'sv', 'ru');
-INSERT INTO translator VALUES (8, 1, 'is', 'ru');
-INSERT INTO translator VALUES (9, 1, 'da', 'ru');
-INSERT INTO translator VALUES (10, 1, 'pl', 'ru');
+INSERT INTO translator(id,service_id,src_lang_id,dest_lang_id) VALUES (1, 0, 'fr', 'ru');
+INSERT INTO translator(id,service_id,src_lang_id,dest_lang_id) VALUES (2, 0, 'de', 'ru');
+INSERT INTO translator(id,service_id,src_lang_id,dest_lang_id) VALUES (3, 1, 'en', 'ru');
+INSERT INTO translator(id,service_id,src_lang_id,dest_lang_id) VALUES (4, 1, 'fr', 'ru');
+INSERT INTO translator(id,service_id,src_lang_id,dest_lang_id) VALUES (5, 1, 'de', 'ru');
+INSERT INTO translator(id,service_id,src_lang_id,dest_lang_id) VALUES (6, 1, 'el', 'ru');
+INSERT INTO translator(id,service_id,src_lang_id,dest_lang_id) VALUES (7, 1, 'sv', 'ru');
+INSERT INTO translator(id,service_id,src_lang_id,dest_lang_id) VALUES (8, 1, 'is', 'ru');
+INSERT INTO translator(id,service_id,src_lang_id,dest_lang_id) VALUES (9, 1, 'da', 'ru');
+INSERT INTO translator(id,service_id,src_lang_id,dest_lang_id) VALUES (10, 1, 'pl', 'ru');
 
 --
--- CREATE  TABLE IF NOT EXISTS `service` (
---   `id` BIGINT NOT NULL AUTO_INCREMENT ,
---   `title` VARCHAR(32) NULL DEFAULT NULL ,
---   `host` VARCHAR(64) NULL DEFAULT NULL ,
---   `req_pattern` VARCHAR(255) NULL DEFAULT NULL ,
---   `res_pattern` VARCHAR(255) NULL DEFAULT NULL ,
+-- CREATE  TABLE IF NOT EXISTS service (
+--   id bigint NOT NULL GENERATED BY DEFAULT AS IDENTITY,
+--   title varchar(32),
+--   host varchar(64),
+--   req_pattern varchar(255),
+--   res_pattern varchar(255));
 --
--- CREATE  TABLE IF NOT EXISTS `translator` (
---   `id` BIGINT NOT NULL AUTO_INCREMENT ,
---   `service_id` BIGINT NOT NULL ,
---   `src_lang_id` VARCHAR(2) NOT NULL ,
---   `dest_lang_id` VARCHAR(2) NOT NULL ,
+-- CREATE  TABLE IF NOT EXISTS translator (
+--   id bigint NOT NULL GENERATED BY DEFAULT AS IDENTITY,
+--   service_id bigint NOT NULL,
+--   src_lang_id varchar(2) NOT NULL ,
+--   dest_lang_id varchar(2) NOT NULL);
 --
