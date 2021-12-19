f274364f49192af9a62421169bd5a27cb891d744
Me <me@localhost>
2021-12-20 02:49:23 +0300

    Authentication added but not yet working

---
+++ src/main/java/org/helico/SecurityConfig.java
@@ -0,0 +1,39 @@
+package org.helico;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers("/*").access("hasRole('READER')")
+            .antMatchers("/login").permitAll()
+            .and()
+            .formLogin()
+            .loginProcessingUrl("/login")
+            .successForwardUrl("/dict");
+    };
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}admin")
+                .roles("USER");
+    }
+
+}
