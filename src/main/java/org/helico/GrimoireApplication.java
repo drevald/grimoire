bf83e41a9ffe3cbf6acf691af087d444cc033171
Me <me@localhost>
2021-12-20 01:57:06 +0300

    jsp activated

--- src/main/java/org/helico/GrimoireApplication.java
+++ src/main/java/org/helico/GrimoireApplication.java
@@ -2,10 +2,23 @@ package org.helico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Arrays;
 
 @SpringBootApplication
-public class GrimoireApplication {
+public class GrimoireApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrimoireApplication.class, args);
