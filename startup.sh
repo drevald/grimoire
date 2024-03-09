5982f5e853a0e979490a834400d3b871be20944a
Denis Dreval <dreval@narod.ru>
2024-03-09 12:08:16 +0300

    type fixed in startup script

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export JAVA_OPTS="$JAVA_OPTS -Xms256m -Xmx256m"
 sed -i "s/port=\"8080\"/port=\"$PORT\"/" /usr/local/tomcat/conf/server.xml
-. /usr/local/tomcat/bin/catalina.sh runcat 
+. /usr/local/tomcat/bin/catalina.sh run
