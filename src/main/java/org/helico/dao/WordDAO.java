66805eaa4c75d9b6606f460c2ef60aa65810e0f6
Denis Dreval <dreval@narod.ru>
2024-10-30 10:49:23 +0300

    cleanup

--- src/main/java/org/helico/dao/WordDAO.java
+++ src/main/java/org/helico/dao/WordDAO.java
@@ -4,13 +4,6 @@ import org.helico.domain.Word;
 
 import java.util.List;
 
-/**
- * Created by Wiley Europe, Training & Educational Systems Russia,
- * Created by Wiley Europe, Training & Educational Systems Russia,
- *
- * @author <a href="mailto:dedreval@wiley.com">ddreval</a>
- * @version 17.05.11
- */
 public interface WordDAO {
 
     public Word store(String word, String langId);
--- src/main/java/org/helico/service/WordService.java
+++ src/main/java/org/helico/service/WordService.java
@@ -5,13 +5,6 @@ import org.helico.domain.Word;
 
 import java.util.List;
 
-/**
- * Created by Wiley Europe, Training & Educational Systems Russia,
- * Created by Wiley Europe, Training & Educational Systems Russia,
- *
- * @author <a href="mailto:dedreval@wiley.com">ddreval</a>
- * @version 17.05.11
- */
 public interface WordService {
 
     public void batchStore(List<Word> words, Long dictId);
